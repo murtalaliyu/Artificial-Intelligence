d595d34656631d3fbb3699f5647c699b0223c441
murtala <murtalaliyu10@yahoo.com>
2017-02-01 15:26:16 -0500

    virtual scrollbar now works

--- Grid.py
+++ Grid.py
@@ -1,6 +1,6 @@
 import Tile
 import Tkinter as tk
-
+'''
 root = tk.Tk()
 
 def draw():
@@ -8,25 +8,66 @@ def draw():
     frame = tk.Frame(root)
     frame.pack(fill=tk.BOTH, expand=1)
 
+    #Create scrollbars
+    scrollbarX = tk.Scrollbar(frame, bg='grey')
+    scrollbarX.activate()
+
     ID = 1
     status = "1"
 
     for row in range(50):
         for col in range(50):
             tile = Tile.make_tile(ID, [row, col], status)
-            cell = tk.Label(frame, text=[row, col], bg="green")
+            cell = tk.Label(frame, text=tile.status, bg="green")
             cell.grid(row=row, column=col, padx='1', pady='1')
             ID += 1
             
 draw()
 root.mainloop()
+'''
 
 
 
 
 
 
+class Example(tk.Frame):
+    def __init__(self, root):
 
+        tk.Frame.__init__(self, root)
+        self.canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
+        self.frame = tk.Frame(self.canvas, background="#ffffff")
+        self.vsb = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((4,4), window=self.frame, anchor="nw", 
+                                  tags="self.frame")
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        self.populate()
+
+    def populate(self):
+        ID = 1
+        status = "1"
+
+        for row in range(50):
+            for col in range(50):
+                tile = Tile.make_tile(ID, [row, col], status)
+                cell = tk.Label(self.frame, text=tile.ID, bg="green")
+                cell.grid(row=row, column=col, padx='1', pady='1')
+                ID += 1
+
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+if __name__ == "__main__":
+    root=tk.Tk()
+    Example(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
 
 
 
