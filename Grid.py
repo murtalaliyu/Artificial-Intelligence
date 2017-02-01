eb33329334a8b714c562adbe2a5d74e74426bd7f
murtala <murtalaliyu10@yahoo.com>
2017-02-01 14:20:09 -0500

    working on grid optimization

--- Grid.py
+++ Grid.py
@@ -4,17 +4,17 @@ import Tkinter as tk
 root = tk.Tk()
 
 def draw():
-    global gameframe
-    gameframe =tk.Frame(root)
-    gameframe.pack()
+    global frame
+    frame =tk.Frame(root)
+    #frame.pack()
 
     ID = 1
     status = "1"
 
-    for row in range(10):
-        for col in range(10):
+    for row in range(120):
+        for col in range(160):
             tile = Tile.make_tile(ID, [row, col], status)
-            L = tk.Label(gameframe, text=status, bg="green")
+            L = tk.Label(frame, text=ID, bg="green")
             L.grid(row=row, column=col, padx='1', pady='1')
             ID += 1
             
