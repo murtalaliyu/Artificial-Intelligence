633e1902bda345994e6abee3b0b98180bff27717
murtala <murtalaliyu10@yahoo.com>
2017-02-05 00:56:48 -0500

    hard to traverse fully functional

--- Grid.py
+++ Grid.py
@@ -97,55 +97,6 @@ class Grid(tk.Frame):
                             cell = tk.Label(self.frame, text=tile.status, bg="yellow")
                             cell.grid(row=x, column=y, padx="1", pady="1", columnspan=1, rowspan=1)
             
-
-
-        
-        '''for coordinate in range(8):
-            Xcoordinate = int(random.random() * 120)
-            Ycoordinate = int(random.random() * 160)
-            print "Xcoordinate: %d" % Xcoordinate
-            print "Ycoordinate: %d" % Ycoordinate
-
-            #tile.address == [Xcoordinate, Ycoordinate]
-            #NEEDS SOME MORE LOOKING INTO
-            #if tile.status == "1":
-            #CONSIDER THE 31x31 REGION CENTERED AT THIS COORDINATE PAIR FIRST 
-            if (Xcoordinate+31) <= 159:
-                col_right = Xcoordinate + 31
-                print col_right
-            else: 
-                col_right = 159
-                print col_right
-            if (Xcoordinate-31) >= 0:
-                col_left = Xcoordinate - 31
-                print col_left
-            else:
-                col_left = 0
-                print col_left
-            if (Ycoordinate+31) <= 119:
-                row_down = Ycoordinate + 31
-                print row_down
-            else:
-                row_down = 119
-                print row_down
-            if (Ycoordinate-31) >= 0:
-                row_up = Ycoordinate - 31
-                print row_up
-            else:
-                row_up = 0
-                print row_up
-                
-            #choose with prob 50% to mark it as a hard to taverse cell
-            for x in range(row_left, row_right+1):
-                for y in range(col_up, col_down+1):
-                    print [x, y]
-                    prob = int(random.random() * 2)
-                    if prob == 1:
-                        tile.address = [x, y]
-                        tile.status = "2"
-                        cell = tk.Label(self.frame, text=tile.status, bg="yellow")
-                        cell.grid(row=x, column=y, padx="1", pady="1", columnspan=1, rowspan=1)'''
-
 if __name__ == "__main__":
     root=tk.Tk()
     Grid(root).pack(side="top", fill="both", expand=True)
