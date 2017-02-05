a83d5ce2c8151b982b4844bf76d481d385cd95cc
murtala <murtalaliyu10@yahoo.com>
2017-02-04 23:39:42 -0500

    hard to traverse functional

--- Grid.py
+++ Grid.py
@@ -50,52 +50,92 @@ class Grid(tk.Frame):
         #return gridList        
 
     #select 8 coordinates randomly (xrand, yrand) & set them as hard to traverse
-    def hard_to_traverse(self):       
-        for coordinate in range(8):
+    def hard_to_traverse(self):
+
+        iterator = 0
+        hardToTraverseList = []
+        
+        while iterator < 8:
+
+            Xcoordinate = int(random.random() * 120)
+            Ycoordinate = int(random.random() * 160)
+            print "Xcoordinate: %d" % Xcoordinate
+            print "Ycoordinate: %d" % Ycoordinate
+            
+            if [Xcoordinate, Ycoordinate] not in hardToTraverseList:
+                
+                iterator += 1
+                hardToTraverseList.append([Xcoordinate, Ycoordinate])
+
+                top_left = Xcoordinate - 31
+                top_right = Ycoordinate + 31
+                bottom_left = Xcoordinate + 31
+
+                #sides-31
+                if (top_left < 0):
+                    top_left = 0
+                if (top_right > 159):
+                    top_right = 159
+                if bottom_left > 120:
+                    bottom_left = 120
+                
+                for x in range(top_left, bottom_left):
+                    for y in range(top_left, top_right):
+                        prob = int(random.random() *2)
+                        if prob == 1:
+                            tile.adress = [x, y]
+                            tile.status = "2"
+                            cell = tk.Label(self.frame, text=tile.status, bg="yellow")
+                            cell.grid(row=x, column=y, padx="1", pady="1", columnspan=1, rowspan=1)
+            
+
+
+        
+        '''for coordinate in range(8):
             Xcoordinate = int(random.random() * 120)
             Ycoordinate = int(random.random() * 160)
             print "Xcoordinate: %d" % Xcoordinate
             print "Ycoordinate: %d" % Ycoordinate
 
-            tile.address == [Xcoordinate, Ycoordinate]
+            #tile.address == [Xcoordinate, Ycoordinate]
             #NEEDS SOME MORE LOOKING INTO
-            if tile.status == "1":
-                #CONSIDER THE 31x31 REGION CENTERED AT THIS COORDINATE PAIR FIRST 
-                if (Xcoordinate+31) <= 159:
-                    row_right = Xcoordinate + 31
-                    print row_right
-                else: 
-                    row_right = 159
-                    print row_right
-                if (Xcoordinate-31) >= 0:
-                    row_left = Xcoordinate - 31
-                    print row_left
-                else:
-                    row_left = 0
-                    print row_left
-                if (Ycoordinate+31) <= 119:
-                    col_down = Ycoordinate + 31
-                    print col_down
-                else:
-                    col_down = 119
-                    print col_down
-                if (Ycoordinate-31) >= 0:
-                    col_up = Ycoordinate - 31
-                    print col_up
-                else:
-                    col_up = 0
-                    print col_up
+            #if tile.status == "1":
+            #CONSIDER THE 31x31 REGION CENTERED AT THIS COORDINATE PAIR FIRST 
+            if (Xcoordinate+31) <= 159:
+                col_right = Xcoordinate + 31
+                print col_right
+            else: 
+                col_right = 159
+                print col_right
+            if (Xcoordinate-31) >= 0:
+                col_left = Xcoordinate - 31
+                print col_left
+            else:
+                col_left = 0
+                print col_left
+            if (Ycoordinate+31) <= 119:
+                row_down = Ycoordinate + 31
+                print row_down
+            else:
+                row_down = 119
+                print row_down
+            if (Ycoordinate-31) >= 0:
+                row_up = Ycoordinate - 31
+                print row_up
+            else:
+                row_up = 0
+                print row_up
                 
-                #choose with prob 50% to mark it as a hard to taverse cell
-                for x in range(row_left, row_right+1):
-                    for y in range(col_up, col_down+1):
-                        print [x, y]
-                        prob = int(random.random() * 2)
-                        if prob == 1:
-                            tile.address = [x, y]
-                            tile.status = "2"
-                            cell = tk.Label(self.frame, text=tile.status, bg="yellow")
-                            cell.grid(row=x, column=y, padx="1", pady="1", columnspan=1, rowspan=1)
+            #choose with prob 50% to mark it as a hard to taverse cell
+            for x in range(row_left, row_right+1):
+                for y in range(col_up, col_down+1):
+                    print [x, y]
+                    prob = int(random.random() * 2)
+                    if prob == 1:
+                        tile.address = [x, y]
+                        tile.status = "2"
+                        cell = tk.Label(self.frame, text=tile.status, bg="yellow")
+                        cell.grid(row=x, column=y, padx="1", pady="1", columnspan=1, rowspan=1)'''
 
 if __name__ == "__main__":
     root=tk.Tk()
